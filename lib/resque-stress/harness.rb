2af5b2306372213f23db2aa39cf6d38541e3e204
lwoodson <lance@webmaneuvers.com>
2013-07-28 10:21:13 -0500

    Added Harness#all_jobs to collect all jobs for all queues.
    They are also sorted by weight descending.

--- lib/resque-stress/harness.rb
+++ lib/resque-stress/harness.rb
@@ -11,6 +11,15 @@ module Resque
         raise 'Name not specified' if name.nil?
         raise 'Name cannot be empty' if name.to_s.empty?
       end
+
+      def all_jobs
+        queues.map(&:jobs).flatten.sort &reverse_by_weight
+      end
+
+      private
+      def reverse_by_weight
+        lambda {|i, j| j.weight <=> i.weight}
+      end
     end
   end
 end
--- spec/resque-stress/harness_spec.rb
+++ spec/resque-stress/harness_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe Resque::Stress::Harness do
+  let(:harness) {Resque::Stress::Harness.new}
   describe "#validate!" do
-    let(:harness) {Resque::Stress::Harness.new}
-
     it "should raise error if name is nil" do
       harness.name = nil
       expect {harness.validate!}.to raise_error
@@ -12,6 +11,39 @@ describe Resque::Stress::Harness do
     it "should raise error if name is empty" do
       harness.name = ''
       expect {harness.validate!}.to raise_error
+    end
+  end
+
+  describe "#all_jobs" do
+    let(:queue1) {Resque::Stress::QueueDef.new}
+    let(:queue2) {Resque::Stress::QueueDef.new}
+    let(:job1) {Resque::Stress::JobDef.new}
+    let(:job2) {Resque::Stress::JobDef.new}
+    let(:job3) {Resque::Stress::JobDef.new}
+
+    before do
+      queue1.name = 'queue1'
+      queue1.jobs << job1
+      queue1.jobs << job3
+
+      queue2.name = 'queue2'
+      queue2.jobs << job2
+
+      job1.weight = 1
+      job2.weight = 4
+      job3.weight = 3
+
+      harness.queues << queue1
+      harness.queues << queue2
+    end
+
+    it "should contain jobs from all queues" do
+      result = Set.new(harness.all_jobs)
+      result.should == Set.new([job1, job2, job3])
+    end
+
+    it "should have jobs sorted according to weight" do
+      harness.all_jobs.should == [job2, job3, job1]
     end
   end
 end
